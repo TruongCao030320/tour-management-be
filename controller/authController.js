80877d012164c0651ba06173546e604e354f32a8
Truong <truongcao3320@gmail.com>
2024-06-14 17:39:42 +0700

    fix middle verify token

--- controller/authController.js
+++ controller/authController.js
@@ -1,57 +1,137 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-export const register = async (req, res) => {
-  try {
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password, salt);
+// export const register = async (req, res) => {
+//   try {
+//     const salt = bcrypt.genSaltSync(10);
+//     const hash = bcrypt.hashSync(req.body.password, salt);
 
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
+//     const newUser = new User({
+//       username: req.body.username,
+//       email: req.body.email,
+//       password: hash,
+//       photo: req.body.photo,
+//     });
+//     await newUser.save();
+//     res.status(200).json({
+//       success: true,
+//       message: "Successfully created",
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       success: true,
+//       message: "Failed to create !",
+//     });
+//   }
+// };
+// export const login = async (req, res) => {
+//   const email = req.body.email;
+//   try {
+//     const user = await User.findOne({ email });
+//     if (!user) {
+//       return res.status(404).json({
+//         success: false,
+//         message: "User not found !!",
+//       });
+//     }
+//     const checkCorrectPassword = await bcrypt.compare(
+//       req.body.password,
+//       user.password
+//     );
+//     if (!checkCorrectPassword) {
+//       return res.status(401).json({
+//         success: false,
+//         message: "Incorrect email or password !",
+//       });
+//     }
+//     const { password, role, ...rest } = user._doc;
+//     //create jwt token
+//     const token = jwt.sign(
+//       { id: user._id, role: user.role },
+//       process.env.JWT__SECRET_KEY,
+//       { expiresIn: "15d" }
+//     );
+//     // set token in the browser cookie and send the response to the client
+//     res
+//       .cookie("accessToken", token, {
+//         httpOnly: true,
+//         expires: token.expiresIn,
+//       })
+//       .status(200)
+//       .json({
+//         success: true,
+//         message: "Successfully login",
+//         token,
+//         data: { ...rest },
+//         role,
+//       });
+//   } catch (error) {
+//     res.status(500).json({
+//       success: false,
+//       message: "Failed to login !",
+//     });
+//   }
+// };
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+  // console.log(req.body);
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+  try {
+    const user = await User.find({ email });
+    console.log("user", user);
+    if (user.length === 1) {
+      return res.status(500).json({
+        success: false,
+        message: "This email already exist.",
+      });
+    }
+    const newUser = User({
+      username,
+      email,
       password: hash,
-      photo: req.body.photo,
     });
     await newUser.save();
     res.status(200).json({
       success: true,
-      message: "Successfully created",
+      message: "Create new user successfully.",
+      data: newUser,
     });
   } catch (error) {
     res.status(500).json({
       success: true,
-      message: "Failed to create !",
+      message: error.message,
     });
   }
 };
 export const login = async (req, res) => {
-  const email = req.body.email;
+  const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(404).json({
         success: false,
-        message: "User not found !!",
+        message: "Your email is not exist",
       });
     }
-    const checkCorrectPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-    if (!checkCorrectPassword) {
-      return res.status(401).json({
+    const hashPassword = await bcrypt.compare(password, user.password);
+    if (!hashPassword) {
+      return res.status(404).json({
         success: false,
-        message: "Incorrect email or password !",
+        message: "Your email or password is wrong !",
       });
     }
-    const { password, role, ...rest } = user._doc;
-    //create jwt token
+    const { username, ...rest } = user._doc;
     const token = jwt.sign(
-      { id: user._id, role: user.role },
+      {
+        username: username,
+        role: user.role,
+      },
       process.env.JWT__SECRET_KEY,
-      { expiresIn: "15d" }
+      {
+        expiresIn: "15d",
+      }
     );
-    // set token in the browser cookie and send the response to the client
     res
       .cookie("accessToken", token, {
         httpOnly: true,
@@ -60,15 +140,14 @@ export const login = async (req, res) => {
       .status(200)
       .json({
         success: true,
-        message: "Successfully login",
-        token,
+        message: "Login Successfully !",
         data: { ...rest },
-        role,
+        role: user.role,
       });
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: "Failed to login !",
+      message: error.message,
     });
   }
 };
--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,26 +1,51 @@
 import jwt from "jsonwebtoken";
-const verifyToken = (req, res, next) => {
+// const verifyToken = (req, res, next) => {
+//   const token = req.cookies.accessToken;
+//   if (!token) {
+//     return res.status(401).json({
+//       success: false,
+//       message: "You're not authorize",
+//     });
+//   }
+//   jwt.verify(token, process.env.JWT__SECRET_KEY, (err, user) => {
+//     if (err) {
+//       return res.status(401).json({
+//         success: false,
+//         message: "Token is invalid",
+//       });
+//     }
+//     req.user = user;
+//     next(); //dont forget to call next
+//   });
+// };
+export const verifyToken = async (req, res, next) => {
   const token = req.cookies.accessToken;
-  if (!token) {
-    return res.status(401).json({
-      success: false,
-      message: "You're not authorize",
-    });
-  }
-  jwt.verify(token, process.env.JWT__SECRET_KEY, (err, user) => {
-    if (err) {
-      return res.status(401).json({
+  try {
+    if (!token) {
+      return res.status(404).json({
         success: false,
-        message: "Token is invalid",
+        message: "You are not authenticate",
       });
     }
-    req.user = user;
-    next(); //dont forget to call next
-  });
+    jwt.verify(token, process.env.JWT__SECRET_KEY, (err, user) => {
+      if (err) {
+        return res.status(500).json({
+          message: false,
+          massage: "Token is invalid or expired !",
+        });
+      }
+      req.user = user;
+      next();
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: false,
+      massage: "Error at verify token!",
+    });
+  }
 };
-
 export const verifyAdmin = (req, res, next) => {
-  verifyToken(req, res, next, () => {
+  verifyToken(req, res, () => {
     if (req.user.role === "admin") {
       next();
     } else {
@@ -31,15 +56,40 @@ export const verifyAdmin = (req, res, next) => {
     }
   });
 };
+// này là function của ng ta
+// export const verifyUser = (req, res, next) => {
+//   verifyToken(req, res, next, () => {
+//     if (req.user.id === req.params.id || req.user.role === "admin") {
+//       next();
+//     } else {
+//       return res.status(401).json({
+//         success: false,
+//         message: "You're not authenticated",
+//       });
+//     }
+//   });
+// };
 
-export const verifyUser = (req, res, next) => {
-  verifyToken(req, res, next, () => {
-    if (req.user.id === req.params.id || req.user.role === "admin") {
+// export const verifyAdmin = async (req, res, next) => {
+//   verifyToken(req, res, next, () => {
+//     if (req.user.role === "admin") {
+//       next();
+//     } else {
+//       return res.status(401).json({
+//         success: false,
+//         message: "You are not authorize!!",
+//       });
+//     }
+//   });
+// };
+export const verifyUser = async (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.role === "user") {
       next();
     } else {
       return res.status(401).json({
         success: false,
-        message: "You're not authenticated",
+        message: "You are not authorize!!",
       });
     }
   });
